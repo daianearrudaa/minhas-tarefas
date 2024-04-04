26ca7a761542bc051e9ecc9c97f089f78e143aac
Daiane Arruda <daianedasilvaarruda@outlook.com>
2024-04-04 12:17:37 -0300

    aplicando React Router DOM

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import BarraLateral from '../../Containers/BarraLateral'
+import ListadeTarefas from '../../Containers/ListadeTarefa'
+
+const Home = () => (
+  <>
+    <BarraLateral />
+    <ListadeTarefas />
+  </>
+)
+
+export default Home
