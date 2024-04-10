5c43337b5728911c7927d4a4510a73713679a25b
Daiane Arruda <daianedasilvaarruda@outlook.com>
2024-04-10 15:08:46 -0300

    criando tarefa

--- src/store/reducers/tarefas.ts
+++ src/store/reducers/tarefas.ts
@@ -46,10 +46,21 @@ const tarefasSlice = createSlice({
       if (indexTarefa >= 0) {
         state.itens[indexTarefa] = action.payload
       }
+    },
+    cadastrar: (state, action: PayloadAction<Tarefa>) => {
+      const tarefaExiste = state.itens.find(
+        (tarefa) =>
+          tarefa.titulo.toLowerCase() === action.payload.titulo.toLowerCase()
+      )
+      if (tarefaExiste) {
+        alert('já existe uma tarefa com esse nome')
+      } else {
+        state.itens.push(action.payload)
+      }
     }
   }
 })
 
-export const { remover, editar } = tarefasSlice.actions
+export const { remover, editar, cadastrar } = tarefasSlice.actions
 
 export default tarefasSlice.reducer
